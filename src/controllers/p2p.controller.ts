36142dbd0cae3676e21eba363f75cfaaa76f21cb
bvanega <braianvanega@gmail.com>
2025-10-30 11:20:31 -0300

    Avances

--- src/controllers/p2p.controller.ts
+++ src/controllers/p2p.controller.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from "next/server";
-import { fetchBinanceOrders } from "@/services/p2p.service";
+import { Spot } from "@binance/connector";
 
-export async function getOrders(request: Request) {
-  const apiKey = process.env.NEXT_PUBLIC_BINANCE_API_KEY;
-  const apiSecret = process.env.NEXT_PUBLIC_BINANCE_API_SECRET;
+const apiKey = process.env.NEXT_PUBLIC_BINANCE_API_KEY;
+const apiSecret = process.env.NEXT_PUBLIC_BINANCE_API_SECRET;
 
-  const fetchOrders = async () => {
-    const orders = await client.getOrders("BTCUSDT");
-    return orders.data;
-  };
+const client = new Spot(apiKey as string, apiSecret as string);
 
-  return fetchOrders();
+export async function getMyOrders(): Promise<any> {
+  try {
+    const response = await client.getOrders("BTCUSDT");
+    return response.data;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error.message);
+  }
 }
